695ba44c70d4267a291111817cded747d90f265e
AQIBYASEENBHAT <AQIBYASEENBHAT@GMAIL.COM>
2022-06-18 12:46:26 +0530

    on click finds the parent node and then child elemnt is retrieved and a style is applied to it

--- script.js
+++ script.js
@@ -4,16 +4,22 @@ let link = document.getElementById("link");
 
 table.addEventListener('click',function(e){
     //retrieves id of the row that was clicked then stores the array of elements of that row inside tar
-    let tar = document.getElementById(e.target.parentNode.parentNode.id).getElementsByTagName('td');
+    let tar = document.getElementById(e.target.parentNode.parentElement.id).getElementsByTagName('td');
     
     /*passing id of clicked item through session storage using index of table data
     sessionStorage.setItem("name",tar[2].innerText);
     */
-   
-   e.target.dataset.nam = tar[0].innerText;
-   e.target.dataset.age = tar[1].innerText;
-   e.target.dataset.idd = tar[2].innerText;
-    console.log(tar[0]);
+
+    e.target.dataset.nam = tar[0].innerText;
+    e.target.dataset.age = tar[1].innerText;
+    e.target.dataset.idd = tar[2].innerText;
+
+    console.log(e.target.parentElement.parentElement.children[e.target.parentElement.parentElement.children.length - 1]);
+    console.log(e.target.nodeName);
+
+    if(e.target.nodeName=="A"){
+    e.target.parentElement.parentElement.children[e.target.parentElement.parentElement.children.length - 1].style.backgroundColor = "green";
+    }
 
     //passing id of clicked item through session storage using data set property
     
@@ -21,13 +27,20 @@ table.addEventListener('click',function(e){
     sessionStorage.setItem("age",e.target.dataset.age);
     sessionStorage.setItem("id",e.target.dataset.idd);
 
-    
+    // link.dataset.nam = tar[0].innerText;
+    // link.dataset.idd = tar[2].innerText;
+    // link.dataset.age = tar[1].innerText;
+
+    // document.getElementById("div1").innerText=link.dataset.nam;
+    // document.getElementById("div1").innerText =` Age : ${link.dataset.age} Name: ${link.dataset.nam} Id ${link.dataset.idd}`;
+    // document.getElementById("div1").innerText=link.dataset.idd;
+
     
     // console.log(x);
     
     
     
-    console.log();
+    // console.log(Math.random());
     
     // sessionStorage.setItem("name",);
     // console.log(sessionStorage.getItem("name"));
